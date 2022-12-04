148f153bac5fab6bc3df60334816512a87bd06e4
Felix Nardmann <felix.nardmann@gmx.de>
2022-12-04 12:14:50 +0100

    update visul

--- main.py
+++ main.py
@@ -67,8 +67,22 @@ x2 = times[-1]
 pos = [(x - x1).days for x in times]
 
 fig, ax = plt.subplots()
-ax.boxplot(intervals, vert=True, positions=pos, widths=5)
-ax.set_xlim([-5, (x2-x1).days + 5])
+
+# gray background with white lines
+ax.set_facecolor('#E6E6E6')
+ax.set_axisbelow(True)
+plt.grid(color='w', linestyle='solid')
+
+boxplots = ax.boxplot(intervals, vert=True, positions=pos, widths=5, patch_artist=True)
+
+# customize boxplots
+for box in boxplots['boxes']:
+    box.set(color='black', linewidth=1) # outline color
+    box.set(facecolor='#EE6666') # fill color
+
+for median in boxplots['medians']:
+    median.set(color='darkred', linewidth=1)
+
 
 # get months of existing range and use them as ticks
 months = []
@@ -93,8 +107,12 @@ labels = [month_dict[x.month] for x in months if x > x1]
 
 ax.set_xticks(ticks)
 ax.set_xticklabels(labels)
+ax.set_xlim([-5, (x2-x1).days + 5])
 
-# invert y axis because lower min/km means faster
-ax.invert_yaxis()
+ax.set_axisbelow(True)
+ax.set_yticks([3.0, 3.167, 3.333, 3.5, 3.667, 3.833, 4.0])
+ax.set_yticklabels(["3:00", "3:10", "3:20", "3:30", "3:40", "3:50", "4:00"])
+ax.set_ylim([2.95, 4.05])
+ax.invert_yaxis() # invert y axis because lower min/km means faster
 
 plt.show()
