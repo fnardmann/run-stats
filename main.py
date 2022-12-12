766b8de7e8689af89e71c32742d1b33bfcb455cd
Felix Nardmann <felix.nardmann@gmx.de>
2022-12-12 21:33:55 +0100

    put functions in utils file

--- main.py
+++ main.py
@@ -2,96 +2,10 @@ import fitparse
 import glob
 import matplotlib.pyplot as plt
 import numpy as np
-import datetime
 from tqdm import tqdm
 
-def tominkm(speeds):
-    """ Convert from m/s to min/km """
-    speeds = speeds * 60 * 60 / 1000 # km/h
-    speeds = np.divide(3600, speeds, out=np.zeros_like(speeds), where=speeds!=0)
-    return speeds / 60 # min/km
-
-
-def extract_lap(fitfile, fields):
-    """ Extract lap information of this fitfile """
-    # dictionary to collect data
-    # key: name, value: data records
-    datadict = {}
-
-    # every field represents a data record to be extracted
-    for field in fields:
-        datadict[field] = []
-
-    # Iterate over all messages of type "record"
-    # (other types include "device_info", "file_creator", "event", etc)
-    for record in fitfile.get_messages('lap'):
-        # Records can contain multiple pieces of data (ex: timestamp, latitude, longitude, etc)
-        for data in record:
-            for field in fields:
-                if data.name == field:
-                    datadict[field].append(data.value)
-
-    # convert to numpy array and ignore None values
-    for key, value in datadict.items():
-        value = np.array(value)
-        datadict[key] = value[value != None]
-    
-    return datadict
-
-def months_in_range(first_ts, last_ts):
-    # get months of existing range and use them as ticks
-    months = []
-    month = datetime.date(first_ts.year, first_ts.month, 1)
-    while month < last_ts:
-        months.append(month)
-        # advance one month
-        new_year = month.year
-        new_month = month.month + 1
-        if new_month > 12:
-            new_year += 1
-            new_month -= 12
-        month = datetime.date(new_year, new_month, 1)
-    return months
-
-def month_ticks_labels(timestamps):
-    months = months_in_range(timestamps[0], timestamps[-1])
-    return month_ticks(months, timestamps[0]), month_labels(months)
-
-def month_ticks(months, first_ts):
-    # create ticks with days from first timestamp
-    return [(x - first_ts).days for x in months if x > x1]
-
-def month_labels(months):
-    month_dict = {
-    1 : "Jan", 2 : "Feb", 3 : "Mar", 4 : "Apr",
-    5 : "May", 6 : "Jun", 7 : "Jul", 8 : "Aug",
-    9 : "Sep", 10 : "Oct", 11 : "Nov", 12 : "Dec" }
-
-    return [month_dict[x.month] for x in months if x > x1]
-
-class IntervalData:
-    def __init__(self, timestamp):
-        self.timestamp = timestamp
-        self.data = {}
-
-class IntervalDataCollection:
-    def __init__(self):
-        self.collection = []
-    
-    def add(self, interval_data):
-        self.collection.append(interval_data)
-
-    def sort(self):
-        self.collection = sorted(self.collection, key=lambda d: d.timestamp)
-
-    def get_timestamps(self):
-        return [d.timestamp for d in self.collection]
-
-    def get_data(self, field):
-        return [d.data[field] for d in self.collection]
-
-    def get_data_mean(self, field):
-        return [np.mean(d) for d in self.get_data(field)]
+import utils
+from utils import IntervalData, IntervalDataCollection
 
 standard_fields = ['total_distance', 'start_time', 'enhanced_avg_speed']
 extra_fields = ['avg_heart_rate']
@@ -106,7 +20,7 @@ for file in tqdm(files, total=len(files)):
     # Load the FIT file
     fitfile = fitparse.FitFile(file)
 
-    lap_data = extract_lap(fitfile, standard_fields + extra_fields)
+    lap_data = utils.extract_lap(fitfile, standard_fields + extra_fields)
 
     lap_distance = lap_data['total_distance']
     lap_speed = lap_data['enhanced_avg_speed']
@@ -115,7 +29,7 @@ for file in tqdm(files, total=len(files)):
 
     # filter interval laps and convert from m/s to min/km
     lap_speed = lap_speed[lap_distance == interval_distance]
-    interval_data.data['pace'] = tominkm(lap_speed)
+    interval_data.data['pace'] = utils.tominkm(lap_speed)
 
     # filter extra fields and append to collection
     for field in extra_fields:
@@ -152,7 +66,7 @@ for box in boxplots['boxes']:
 for median in boxplots['medians']:
     median.set(color='navy', linewidth=1)
 
-ticks, labels = month_ticks_labels(timestamps)
+ticks, labels = utils.month_ticks_labels(timestamps)
 
 ax.set_xticks(ticks)
 ax.set_xticklabels(labels)
